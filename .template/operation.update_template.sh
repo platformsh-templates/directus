3a9b10074998e116a79ec86c71c7a46dd61cbbc5
chadcarlson <chad.carlson@platform.sh>
2021-11-02 08:34:47 -0400

    sut & wheel in op.

--- .template/operation.update_template.sh
+++ .template/operation.update_template.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+pip3 install setuptools
+pip3 install wheel
 pip3 install git+git://github.com/platformsh/template-builder.git#egg=template-builder
+
+auto-update platform install_cli
 auto-update $TEMPLATE_PROFILE update
+
 date > last_updated_on
+
 git add .
 git commit -m "auto-update" --allow-empty
