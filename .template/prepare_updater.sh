2d3ababedbae7873679cecdd8aa2866174578440
chadcarlson <chad.carlson@platform.sh>
2021-11-02 10:27:39 -0400

    no tokens on clone.

--- .template/prepare_updater.sh
+++ .template/prepare_updater.sh
@@ -3,5 +3,5 @@
 # Prepare the auto-update tools.
 pip3 install setuptools
 pip3 install wheel
-pip3 install git+git://github.com/platformsh/template-builder.git#egg=template-builder
+pip3 install git+https://github.com/platformsh/template-builder.git#egg=template-builder
 auto-update platform install_cli
