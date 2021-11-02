0d00138a13c290a6a498743ea204fa0e2603c6c9
chadcarlson <chad.carlson@platform.sh>
2021-11-02 15:50:56 -0400

    Remove install from build hook.

--- .platform-scripts/crons.update.sh
+++ .platform-scripts/crons.update.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 if [ $PLATFORM_ENVIRONMENT_TYPE = production ]; then
+    # Prepare the auto-update tools.
+    .platform-scripts/prepare_updater.sh
+
+    # Update the template.
     timeout 1800 auto-update $TEMPLATE_PROFILE trigger_update_dependencies;
 fi;
--- .platform-scripts/operation.update_template.sh
+++ .platform-scripts/operation.update_template.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Prepare the auto-update tools.
-pip3 install setuptools
-pip3 install wheel
-pip3 install git+https://github.com/platformsh/template-builder.git#egg=template-builder
-auto-update platform install_cli
+.platform-scripts/prepare_updater.sh
 
 # Run the update.
 auto-update $TEMPLATE_PROFILE update
