01a3f6dab7737402ced18e44596102d1b05b9018
chadcarlson <chad.carlson@platform.sh>
2021-11-02 10:32:35 -0400

    remove again, keep in operation.

--- .template/operation.update_template.sh
+++ .template/operation.update_template.sh
@@ -3,8 +3,8 @@
 # Prepare the auto-update tools.
 pip3 install setuptools
 pip3 install wheel
-pip3 install git+git://github.com/platformsh/template-builder.git#egg=template-builder
-# auto-update platform install_cli
+pip3 install git+https://github.com/platformsh/template-builder.git#egg=template-builder
+auto-update platform install_cli
 
 # Run the update.
 auto-update $TEMPLATE_PROFILE update
