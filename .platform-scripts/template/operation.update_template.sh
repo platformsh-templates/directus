c63fa2db7d953b02557fdf28ea710f6bfe819170
chadcarlson <chad.carlson@platform.sh>
2021-11-03 11:49:50 -0400

    Move scripts.

--- .platform-scripts/template/operation.update_template.sh
+++ .platform-scripts/template/operation.update_template.sh
@@ -5,7 +5,7 @@ if [ -z ${PLATFORMSH_CLI_TOKEN+x} ]; then
 else 
     if [ $PLATFORM_ENVIRONMENT_TYPE = production ]; then
         # Prepare the auto-update tools.
-        .platform-scripts/prepare_updater.sh
+        .platform-scripts/template/prepare_updater.sh
 
         # Verify project.
         USERS=$(platform project:curl access | jq -c 'map(select(."_embedded".users[0].email | contains("devrel@internal.platform.sh")))')
