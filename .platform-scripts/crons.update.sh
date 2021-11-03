412ddb42259a7dc1fbe01f2c1906c6f94f569f02
chadcarlson <chad.carlson@platform.sh>
2021-11-03 10:00:37 -0400

    Include p.sh checks for maintenance.

--- .platform-scripts/crons.update.sh
+++ .platform-scripts/crons.update.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-if [ $PLATFORM_ENVIRONMENT_TYPE = production ]; then
-    # Prepare the auto-update tools.
-    .platform-scripts/prepare_updater.sh
+if [ -z ${PLATFORMSH_CLI_TOKEN+x} ]; then 
+    echo "PLATFORMSH_CLI_TOKEN is undefined. Skipping installation."; 
+else 
+    if [ $PLATFORM_ENVIRONMENT_TYPE = production ]; then
+        # Prepare the auto-update tools.
+        .platform-scripts/prepare_updater.sh
 
-    # Update the template.
-    timeout 1800 auto-update $TEMPLATE_PROFILE trigger_update_dependencies;
-fi;
+        # Verify project.
+        USERS=$(platform project:curl access | jq -c 'map(select(."_embedded".users[0].email | contains("devrel@internal.platform.sh")))')
+        MATCH_USERS=$(echo $USERS | jq -r 'length')
+        if [ $MATCH_USERS == 0 ]; then
+            echo "Skipping template maintenance."
+            echo "See the instructions for adding automatic updates to your project:"
+            echo "  -> https://community.platform.sh/t/fully-automated-dependency-updates-with-source-operations/801"
+        else
+            # Update the template.
+            timeout 1800 auto-update $TEMPLATE_PROFILE trigger_update_dependencies;
+        fi
+    fi;
+fi
