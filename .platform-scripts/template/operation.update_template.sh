0a40be8664359f95e138b4d0b54474fa9e2f27a2
chadcarlson <chad.carlson@platform.sh>
2021-11-04 12:00:23 -0400

    Update cron + tools download.

--- .platform-scripts/template/operation.update_template.sh
+++ .platform-scripts/template/operation.update_template.sh
@@ -3,10 +3,14 @@
 if [ -z ${PLATFORMSH_CLI_TOKEN+x} ]; then 
     echo "PLATFORMSH_CLI_TOKEN is undefined. Skipping operation."; 
 else 
+    # Install the tools
+    .platform-scripts/template/verify_updater.sh
+    
     # Verify project.
+    source ~/.environment
     USERS=$(platform project:curl access | jq -c 'map(select(."_embedded".users[0].email | contains("devrel@internal.platform.sh")))')
     MATCH_USERS=$(echo $USERS | jq -r 'length')
-    if [ $MATCH_USERS == 0 ]; then
+    if [ "$MATCH_USERS" = 0 ]; then
         echo "Skipping template maintenance."
         echo "See the instructions for adding automatic updates to your project:"
         echo "  -> https://community.platform.sh/t/fully-automated-dependency-updates-with-source-operations/801"
